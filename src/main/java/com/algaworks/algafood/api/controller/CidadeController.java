2bd54112d3d070f8c02566a83c555f5f299d9f0c
marcelobonfim <marceloras@gmail.com>
2021-07-06 19:17:34 -0300

    Adicionando constraint de validação no modelo

--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -2,6 +2,8 @@ package com.algaworks.algafood.api.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -43,7 +45,7 @@ public class CidadeController {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cidade salvar(@RequestBody Cidade cidade) {
+	public Cidade salvar(@RequestBody @Valid Cidade cidade) {
 		try {
 			return cidadeService.salvar(cidade);
 		} catch (EstadoNaoEncontradoException e) {
@@ -52,7 +54,7 @@ public class CidadeController {
 	}
 
 	@PutMapping("/{id}")
-	public Cidade atualizar(@PathVariable Long id, @RequestBody Cidade cidade) {
+	public Cidade atualizar(@PathVariable Long id, @RequestBody @Valid Cidade cidade) {
 		try {
 			Cidade cidadeAtual = cidadeService.buscarOuFalhar(id);
 			
