1b9f725e355064d1142d71d11a69043a07181253
marcelobonfim <marceloras@gmail.com>
2021-10-07 14:53:47 -0300

    Cache de HTTP

--- src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
@@ -1,11 +1,15 @@
 package com.algaworks.algafood.api.controller;
 
+import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 import com.algaworks.algafood.api.mapper.FormaPagamentoInputMapper;
 import com.algaworks.algafood.api.mapper.FormaPagamentoResponseMapper;
@@ -27,46 +33,87 @@ import com.algaworks.algafood.domain.service.FormaPagamentoService;
 @RestController
 @RequestMapping("/formas-pagamento")
 public class FormaPagamentoController {
-	
+
 	@Autowired
 	private FormaPagamentoRepository formaPagamentoRepository;
-	
+
 	@Autowired
 	private FormaPagamentoService formaPagamentoService;
-	
+
 	@Autowired
 	private FormaPagamentoResponseMapper formaPagamentoResponseMapper;
-	
+
 	@Autowired
 	private FormaPagamentoInputMapper formaPagamentoInputMapper;
-	
+
 	@GetMapping
-	public List<FormaPagamentoResponse> listar() {
-		return formaPagamentoResponseMapper.toCollectionResponse(formaPagamentoRepository.findAll());
+	public ResponseEntity<List<FormaPagamentoResponse>> listar(ServletWebRequest request) {
+		ShallowEtagHeaderFilter.disableContentCaching(request.getRequest());
+
+		String eTag = "0";
+
+		OffsetDateTime dataUltimaAtualizacao = formaPagamentoRepository.getDataUltimaAtualizacao();
+
+		if (dataUltimaAtualizacao != null) {
+			eTag = String.valueOf(dataUltimaAtualizacao.toEpochSecond());
+		}
+
+		if (request.checkNotModified(eTag)) {
+			return null;
+		}
+
+		List<FormaPagamentoResponse> formasPagamentosResponse = formaPagamentoResponseMapper
+				.toCollectionResponse(formaPagamentoRepository.findAll());
+
+		return ResponseEntity.ok()
+//					.cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+//					.cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePrivate())
+				.cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePublic())
+//					.cacheControl(CacheControl.noCache())
+//					.cacheControl(CacheControl.noStore())
+				.eTag(eTag).body(formasPagamentosResponse);
 	}
-	
+
 	@GetMapping("/{id}")
-	public FormaPagamentoResponse buscar(@PathVariable Long id) {
-		return formaPagamentoResponseMapper.toResponse(formaPagamentoService.buscarOuFalhar(id));
+	public ResponseEntity<FormaPagamentoResponse> buscar(@PathVariable Long id, ServletWebRequest request) {
+		ShallowEtagHeaderFilter.disableContentCaching(request.getRequest());
+
+		String eTag = "0";
+
+		OffsetDateTime dataUltimaAtualizacao = formaPagamentoRepository.getDataUltimaAtualizacaoById(id);
+
+		if (dataUltimaAtualizacao != null) {
+			eTag = String.valueOf(dataUltimaAtualizacao.toEpochSecond());
+		}
+
+		if (request.checkNotModified(eTag)) {
+			return null;
+		}
+		
+		FormaPagamentoResponse formaPagamentoResponse = formaPagamentoResponseMapper
+				.toResponse(formaPagamentoService.buscarOuFalhar(id));
+
+		return ResponseEntity.ok().cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS)).eTag(eTag).body(formaPagamentoResponse);
 	}
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public FormaPagamentoResponse adicionar(@RequestBody @Valid FormaPagamentoInput formaPagamentoInput) {
 		FormaPagamento formaPagamento = formaPagamentoInputMapper.toDomain(formaPagamentoInput);
-		
+
 		return formaPagamentoResponseMapper.toResponse(formaPagamentoService.salvar(formaPagamento));
 	}
-	
+
 	@PutMapping("/{id}")
-	public FormaPagamentoResponse atualizar(@PathVariable Long id, @RequestBody @Valid FormaPagamentoInput formaPagamentoInput) {
+	public FormaPagamentoResponse atualizar(@PathVariable Long id,
+			@RequestBody @Valid FormaPagamentoInput formaPagamentoInput) {
 		FormaPagamento formaPagamentoAtual = formaPagamentoService.buscarOuFalhar(id);
-		
+
 		formaPagamentoInputMapper.fromInputToDomain(formaPagamentoInput, formaPagamentoAtual);
-		
+
 		return formaPagamentoResponseMapper.toResponse(formaPagamentoService.salvar(formaPagamentoAtual));
 	}
-	
+
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void remover(@PathVariable Long id) {
--- src/main/java/com/algaworks/algafood/core/web/Webconfig.java
+++ src/main/java/com/algaworks/algafood/core/web/Webconfig.java
@@ -1,16 +1,24 @@
 package com.algaworks.algafood.core.web;
 
+import javax.servlet.Filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class Webconfig implements WebMvcConfigurer {
-	
-	 @Override
+
+	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-				.allowedMethods("*");
+		registry.addMapping("/**").allowedMethods("*");
 //				.allowedOrigins("*")
 //				.maxAge(30);
+	}
+
+	@Bean
+	public Filter shallowEtagHeaderFilter() {
+		return new ShallowEtagHeaderFilter();
 	}
 
 }
